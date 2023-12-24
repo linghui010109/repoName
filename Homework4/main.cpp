77d3676419bd61562391b80d7f8e58b829af2c9b
linghui010109 <113741642+linghui010109@users.noreply.github.com>
2023-12-24 22:21:11 +0800

    Update main.cpp

--- Homework4/main.cpp
+++ Homework4/main.cpp
@@ -1,42 +1,69 @@
-#include<iostream>
-#include<vector>
-#include<climits>
+#include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
-int minCoins(int amount, const vector<int>& coins)
+int minCoins(int amount, const vector<pair<int, int>>& coinInfo)
 {
-	vector<int> dp(amount + 1, INT_MAX);
-	dp[0] = 0;
-	for (int i : coins)
-	{
-		for (int j = i; j <= amount; j++)
-		{
-			if (dp[j - 1] != INT_MAX)
-			{
-				dp[j] = min(dp[j],dp[j-i]+1);
-			}
-			
-		}
-	}
-	return (dp[amount]==INT_MAX)?-1:dp[amount];
+    vector<int> dp(amount + 1, INT_MAX);
+    dp[0] = 0;
+
+    for (const auto& coin : coinInfo)
+    {
+        int coinDenomination = coin.first;
+        int coinCount = coin.second;
+
+        for (int j = coinDenomination; j <= amount; ++j)
+        {
+            if (dp[j - coinDenomination] != INT_MAX)
+            {
+                dp[j] = min(dp[j], dp[j - coinDenomination] + 1);
+            }
+        }
+    }
+
+    if (dp[amount] == INT_MAX) {
+        return -1;
+    }
+    else {
+        return dp[amount];
+    }
 }
 
 int main()
 {
-	int amount = 63;
-	vector<int> coins = { 1,5,10,25 };
+    int amount;
+    cout << "Enter the amount: ";
+    cin >> amount;
 
-	int result = minCoins(amount, coins);
+    int numCoins;
+    cout << "Enter the number of different coin denominations: ";
+    cin >> numCoins;
 
-	if (result != -1)
-	{
-		cout << "The minimum number of coins to make " << amount << " cents is: " << result << " coins" << endl;
-	}
-	else
-	{
-		cout << "Unable to make " << amount << " cents" << endl;
-	}
+    vector<pair<int, int>> coinInfo;
 
-	return 0;
+    cout << "Enter the coin denominations and counts:" << endl;
+    for (int i = 0; i < numCoins; ++i)
+    {
+        int denomination, count;
+        cout << "Coin " << i + 1 << " denomination: ";
+        cin >> denomination;
+        cout << "Coin " << i + 1 << " count: ";
+        cin >> count;
+        coinInfo.emplace_back(denomination, count);
+    }
+
+    int result = minCoins(amount, coinInfo);
+
+    if (result != -1)
+    {
+        cout << "The minimum number of coins to make " << amount << " cents is: " << result << " coins" << endl;
+    }
+    else
+    {
+        cout << "Unable to make " << amount << " cents" << endl;
+    }
+
+    return 0;
 }
